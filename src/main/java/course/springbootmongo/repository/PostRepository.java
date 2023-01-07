0990667480be73dca4105c93735dbe34cb348c85
Aldrin Ricardo <aldrinricardo96@gmail.com>
2023-01-07 17:00:36 -0300

    Consulta com vários critérios

--- src/main/java/course/springbootmongo/repository/PostRepository.java
+++ src/main/java/course/springbootmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package course.springbootmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -9,8 +10,13 @@ import course.springbootmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, "
+			+ "{ 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
+
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text); //query method
 }
+
